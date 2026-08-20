ebded58969c6e726af844b65c25bc482d169b951
hellokevintaylor-ui <hellokevintaylor@gmail.com>
2026-08-20 16:47:10 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  build: { minify: false },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
